6cf9408e90135815c2a7d12695ceead4f556fd89
ErdemCankayaa <96151728+ErdemCankayaa@users.noreply.github.com>
2023-12-22 05:56:51 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,95 +1,29 @@
-// Create web server 
+// Create web server
 
-// Import modules
+// Import module
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Comment = require('./models/comment');
 const app = express();
-const port = 3000;
 
-// Connect to database
+// Set view engine
+app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost:27017/comment', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Middleware
-app.use(bodyParser.json());
+// Use static file
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Create a new comment
-app.post('/comment', (req, res) => {
-    const comment = new Comment({
-        name: req.body.name,
-        email: req.body.email,
-        comment: req.body.comment
-    });
-    comment.save()
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send('Internal server error');
-        });
-});
+// Import route
+const commentRoute = require('./routes/comment.route');
 
-// Get all comments
+// Use route
+app.use('/', commentRoute);
 
-app.get('/comment', (req, res) => {
-    Comment.find()
-        .then(comment => {
-            res.send(comment);
-        })
-        .catch(err => {
-            res.status(500).send('Internal server error');
-        });
-}
-);
-
-// Get a comment by id
-
-app.get('/comment/:id', (req, res) => {
-    Comment.findById(req.params.id)
-        .then(comment => {
-            res.send(comment);
-        })
-        .catch(err => {
-            res.status(500).send('Internal server error');
-        });
-}
-);
-
-// Update a comment by id
-
-app.put('/comment/:id', (req, res) => {
-    Comment.findByIdAndUpdate(req.params.id, {
-        name: req.body.name,
-        email: req.body.email,
-        comment: req.body.comment
-    })
-        .then(comment => {
-            res.send(comment);
-        })
-        .catch(err => {
-            res.status(500).send('Internal server error');
-        });
-}
-);
-
-// Delete a comment by id
-
-app.delete('/comment/:id', (req, res) => {
-    Comment.findByIdAndDelete(req.params.id)
-        .then(comment => {
-            res.send(comment);
-        })
-        .catch(err => {
-            res.status(500).send('Internal server error');
-        });
-}
-);
-
-// Start server
+// Listen port
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+    console.log(`Server is running on port ${port}...`);
 });
+
+
+
 
